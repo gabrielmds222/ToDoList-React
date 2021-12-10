429f5100dc2c7b462b38911b95e190a9268ea5bb
Gabriel <gabrielmedsilva@outlook.com>
2021-12-09 23:08:55 -0300

    adjust files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>TÁ RODANDO</h1>
+    </div>
+  );
+}
+
+export default App;
