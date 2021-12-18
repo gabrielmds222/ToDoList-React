4faa3a4448dbb7cafa5488ecf4d169f9cb1c8372
Gabriel <gabrielmedsilva@outlook.com>
2021-12-17 22:38:34 -0300

    add button

--- src/components/AddTarefa.jsx
+++ src/components/AddTarefa.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
 import "./AddTarefa.css"
+import Button from "./Button";
 
 const AddTarefa = () => {
     return (
         <div className="add-task-container">
             <input type="text" className="add-task-input"/>
+            <div className="add-task-button-container">
+            <Button> Adicionar </Button>
+            </div>
         </div>
     );
 };
